fda865edda410cd84487a0597ea371f230e4f20d
Grajdhar <rajdharg@gmail.com>
2018-06-10 23:35:00 +0530

    Test message code

--- GrangularProject/src/app/app.component.ts
+++ GrangularProject/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  message : String = "This is a test message";
 }
--- GrangularProject/src/app/app.module.ts
+++ GrangularProject/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent
   ],
   imports: [
     BrowserModule
